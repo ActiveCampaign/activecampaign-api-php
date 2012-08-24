f0634e4f84197d8cabfcd688766a64b4b3d8d778
Matt Thommes <matt@activecampaign.com>
2012-08-24 15:32:11 -0500

    added subscriber_sync function

--- includes/Subscriber.class.php
+++ includes/Subscriber.class.php
@@ -47,6 +47,13 @@ class AC_Subscriber extends ActiveCampaign {
 		return $response;
 	}
 
+	function sync($params, $post_data) {
+		$request_url = "{$this->url}&api_action=subscriber_sync&api_output={$this->output}";
+		if ($params) $request_url .= "&{$params}";
+		$response = $this->curl($request_url, $post_data);
+		return $response;
+	}
+
 	function view($params) {
 		// can be a subscriber ID, email, or hash
 		if (preg_match("/^email=/", $params)) {
