c4e328f697aba93a917f35b2090624e8689f06b8
Matt Thommes <matt@activecampaign.com>
2013-03-18 11:08:38 -0500

    make sure $v is a string - we can't handle another nested array here

--- includes/Connector.class.php
+++ includes/Connector.class.php
@@ -87,8 +87,10 @@ class AC_Connector {
 							// normally we just want the key to be a string, IE: ["group[2]"] => 2
 							// but we want to allow passing both formats
 							foreach ($value as $k => $v) {
-								$k = urlencode($k);
-								$data .= "{$key}[{$k}]=" . urlencode($v) . "&";
+								if (is_string($v)) {
+									$k = urlencode($k);
+									$data .= "{$key}[{$k}]=" . urlencode($v) . "&";
+								}
 							}
 						}
 
