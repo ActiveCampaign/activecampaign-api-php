bba2158ee5feff38065e6d3cb889b1a536108b5e
Matt Thommes <matt@activecampaign.com>
2014-05-30 16:10:52 -0500

    revert the Composer removals for now, until it's tested more

--- includes/ActiveCampaign.class.php
+++ includes/ActiveCampaign.class.php
@@ -4,6 +4,8 @@ if ( !defined("ACTIVECAMPAIGN_URL") || (!defined("ACTIVECAMPAIGN_API_KEY") && !d
 	require_once(dirname(__FILE__) . "/config.php");
 }
 
+require_once("Connector.class.php");
+
 class ActiveCampaign extends AC_Connector {
 
 	public $url_base;
@@ -105,5 +107,21 @@ class ActiveCampaign extends AC_Connector {
 	}
 
 }
+
+require_once("Account.class.php");
+require_once("Auth.class.php");
+require_once("Automation.class.php");
+require_once("Campaign.class.php");
+require_once("Contact.class.php");
+require_once("Design.class.php");
+require_once("Form.class.php");
+require_once("Group.class.php");
+require_once("List.class.php");
+require_once("Message.class.php");
+require_once("Settings.class.php");
+require_once("Subscriber.class.php");
+require_once("Tracking.class.php");
+require_once("User.class.php");
+require_once("Webhook.class.php");
 
 ?>
