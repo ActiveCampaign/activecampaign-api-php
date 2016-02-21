f2291d9f42c000b913062aff1ef31b29ba1f60c0
Matt Thommes <matt@activecampaign.com>
2016-02-21 14:23:10 -0600

    This method will be version 2 only. [#114038301]

--- includes/Segment.class.php
+++ includes/Segment.class.php
@@ -15,13 +15,9 @@ class AC_Segment extends ActiveCampaign {
 	}
 
 	function list_($params) {
-		if ($this->version == 1) {
-			$request_url = "{$this->url}&api_action=segment_list&api_output={$this->output}&{$params}";
-			$response = $this->curl($request_url);
-		} elseif ($this->version == 2) {
-			$request_url = "{$this->url_base}/segment/list";
-			$response = $this->curl($request_url, $params, "GET", "segment_list");
-		}
+		// version 2 only
+		$request_url = "{$this->url_base}/segment/list";
+		$response = $this->curl($request_url, $params, "GET", "segment_list");
 		return $response;
 	}
 
