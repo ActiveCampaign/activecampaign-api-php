f7088ea059e7d72cc29270e0d068916283608e51
Mark Cianfrani <mark@Marks-MacBook-Pro.local>
2017-05-16 12:58:01 -0400

    Added Organization Class

---
+++ includes/Organization.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class AC_Organization extends ActiveCampaign {
+
+    public $version;
+    public $url_base;
+    public $url;
+    public $api_key;
+
+    function __construct($version, $url_base, $url, $api_key) {
+        $this->version = $version;
+        $this->url_base = $url_base;
+        $this->url = $url;
+        $this->api_key = $api_key;
+    }
+
+    function list_($params, $post_data) {
+        $request_url = "{$this->url}&api_action=organization_list&api_output={$this->output}";
+        $response = $this->curl($request_url);
+        return $response;
+    }
+
+}
+
+?>
